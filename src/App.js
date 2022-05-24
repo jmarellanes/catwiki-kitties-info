5d2b525ac243f7f4be36e97a19f3b0b6a098b8d7
José Manuel Díaz <jm@docevoltios.com>
2022-05-24 02:57:55 -0600

    Add axios and test endpoint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import axios from 'axios';
+
 function App() {
+	axios({
+		method: 'get',
+		url: 'https://api.thecatapi.com/v1/favourites',
+		headers: {
+			'x-api-key': process.env.REACT_APP_API_KEY,
+		},
+	}).then(function (response) {
+		console.log(response);
+	});
+
 	return <div>I'm Alive</div>;
 }
 
