6b40491ff6e4a9d90fe78cb000b03f7a2a8aa570
José Manuel Díaz <jm@docevoltios.com>
2022-05-25 17:08:08 -0600

    Add h1 heading

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,9 @@ import styles from './header.module.scss';
 function Header() {
 	return (
 		<header className={styles.header}>
+			<h1 className='visually-hidden'>
+				CatWiki - Get to know more about your cat breed
+			</h1>
 			<div className={styles.header__container}>
 				<Link to='/' className={styles.header__brand}>
 					<Logo />
