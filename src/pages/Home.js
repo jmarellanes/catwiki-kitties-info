0c1e228b7f4ccc419bb7819b1992bc6eeb6c954e
José Manuel Díaz <jm@docevoltios.com>
2022-05-25 02:21:25 -0600

    Add container and styles to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+import Header from 'components/Header';
+import styles from './home.module.scss';
+
 function Home() {
-	return <h1>I'm the home page</h1>;
+	return (
+		<div id={styles.home__wrapper}>
+			<Header />
+			<main>I'm the main content</main>
+			<footer>I'm the footer</footer>
+		</div>
+	);
 }
 
 export default Home;
