441a4c4ca042f1d31e98df3d7481bb9f2c150d9d
José Manuel Díaz <jm@docevoltios.com>
2022-05-25 18:47:46 -0600

    Add markup and styles for Hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,20 @@
+import { ReactComponent as Logo } from 'assets/images/catwiki--logo.svg';
+import styles from './hero.module.scss';
+
+function Hero() {
+	return (
+		<section className={styles.hero}>
+			<div className={styles.hero__container}>
+				<div className={styles.hero__logo}>
+					<Logo />
+				</div>
+
+				<p className={`${styles.hero__text} h1`}>
+					Get to know more about your cat breed
+				</p>
+			</div>
+		</section>
+	);
+}
+
+export default Hero;
