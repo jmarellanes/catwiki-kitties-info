01b29813e0710bb355f06212374e17e5b8e91c79
José Manuel Díaz <jm@docevoltios.com>
2022-05-25 18:28:44 -0600

    Simplify SVG vector

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import Header from 'components/Header';
+import Hero from 'components/Hero';
 import styles from './home.module.scss';
 
 function Home() {
 	return (
 		<div id={styles.home__wrapper}>
 			<Header />
-			<main>I'm the main content</main>
-			<footer>I'm the footer</footer>
+			<main>
+				<Hero />
+			</main>
+			<footer className='visually-hidden'>I'm the footer</footer>
 		</div>
 	);
 }
