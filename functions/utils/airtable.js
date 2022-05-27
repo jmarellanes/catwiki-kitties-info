ae2457dee776ca8a6988d3cab2dd39532c04204d
José Manuel Díaz <jm@docevoltios.com>
2022-05-26 19:24:01 -0600

    Simplify airtable.js

--- functions/utils/airtable.js
+++ functions/utils/airtable.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 const Airtable = require('airtable');
 
-Airtable.configure({
-	apiKey: process.env.AIRTABLE_API_KEY,
-});
-const base = require('airtable').base(process.env.AIRTABLE_BASE);
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+	process.env.AIRTABLE_BASE,
+);
 const catsTable = base(process.env.AIRTABLE_TABLE_MAIN);
 
 module.exports = { catsTable };
