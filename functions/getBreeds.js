ab4c3b690374baa3ba36cf074a401fd8aee7e672
José Manuel Díaz <jm@docevoltios.com>
2022-05-26 19:25:18 -0600

    Add serverless fn to get breeds from DB

---
+++ functions/getBreeds.js
@@ -0,0 +1,45 @@
+// Net
+const { catsTable } = require('./utils/airtable');
+
+exports.handler = async event => {
+	try {
+		const breedsRecords = await catsTable
+			.select({
+				fields: [
+					'Name',
+					'ID',
+					'PopularityRank',
+					'Description',
+					'Temperament',
+					'Origin',
+					'LifeSpan',
+					'Adaptability',
+					'Affection',
+					'ChildFriendly',
+					'Grooming',
+					'Intelligence',
+					'Health',
+					'SocialNeeds',
+					'StrangerFriendly',
+				],
+			})
+			.all();
+		const breedsList = breedsRecords.map(breed => ({
+			fields: breed.fields,
+		}));
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify({
+				breedsList,
+			}),
+		};
+	} catch (err) {
+		console.log(err);
+
+		return {
+			statusCode: 500,
+			body: JSON.stringify(err),
+		};
+	}
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,21 @@ import Header from 'components/Header';
 import Hero from 'components/Hero';
 import styles from './home.module.scss';
 
+import axios from 'axios';
+import { useEffect } from 'react';
+
 function Home() {
+	const getBreed = async () => {
+		let res = await axios.get('/api/getBreeds');
+
+		const datos = res.data;
+		console.log(datos);
+	};
+
+	useEffect(() => {
+		getBreed();
+	});
+
 	return (
 		<div id={styles.home__wrapper}>
 			<Header />
