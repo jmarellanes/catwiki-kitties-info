2f64b0f6e093f1ba17db24a84646543567cd3d83
José Manuel Díaz <jm@docevoltios.com>
2022-05-28 01:46:12 -0600

    Refactor component

--- src/components/SectionLink.js
+++ src/components/SectionLink.js
@@ -3,10 +3,10 @@ import { ReactComponent as ArrowRight } from 'assets/images/icon--arrow-long.svg
 
 import styles from 'styles/modules/see-more-link.module.scss';
 
-export default function SeeMoreLink({ linkTo }) {
+export default function SectionLink({ children, linkTo }) {
 	return (
 		<Link to={linkTo} className={styles.link}>
-			See More <ArrowRight />
+			{children} <ArrowRight />
 		</Link>
 	);
 }
