c7c0af96d108cfba9fd3190e3eea4b5fec2e8d64
José Manuel Díaz <jm@docevoltios.com>
2022-05-27 19:04:54 -0600

    Delete unused code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,9 @@
-import { useGetBreeds } from 'hooks/useGetBreeds';
-
 import BreedsList from 'components/BreedsList';
 import Header from 'components/Header';
 import Hero from 'components/Hero';
 import styles from './home.module.scss';
 
 function Home() {
-	const { data } = useGetBreeds();
-	console.log(data);
-
 	return (
 		<div id={styles.home__wrapper}>
 			<Header />
