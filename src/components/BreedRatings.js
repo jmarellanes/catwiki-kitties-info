fd5e394d2933c6b78b0a16b05cdc3fe6205859ea
José Manuel Díaz <jm@docevoltios.com>
2022-05-31 17:13:26 -0600

    Add markup and styles for Breed Details page

---
+++ src/components/BreedRatings.js
@@ -0,0 +1,20 @@
+import styles from 'styles/modules/page-breed-details.module.scss';
+
+function BreedRatings({ children, value }) {
+	return (
+		<>
+			<dt className={styles['info__group-title']}>{children}:</dt>
+			<dd className={styles['info__attr-system']}>
+				<div className={`${styles[`info__attr-system--value-${value}`]}`}>
+					<span className={styles['info__attr-system--item']}></span>
+					<span className={styles['info__attr-system--item']}></span>
+					<span className={styles['info__attr-system--item']}></span>
+					<span className={styles['info__attr-system--item']}></span>
+					<span className={styles['info__attr-system--item']}></span>
+				</div>
+			</dd>
+		</>
+	);
+}
+
+export default BreedRatings;
