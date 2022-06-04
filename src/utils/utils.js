c079820dd5e16bcbe9a5ead90c5697cc0f0dcb4a
José Manuel Díaz <jm@docevoltios.com>
2022-06-03 21:50:12 -0600

    Change the number of total cat breeds

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 export function getRandomRangeToShow(breedsToShow) {
-	const BREEDS = { TOTAL: 21 };
+	const BREEDS = { TOTAL: 67 };
 	const numberOfBreeds = BREEDS.TOTAL - breedsToShow;
 
 	if (!breedsToShow) return { minRange: 0, maxRange: BREEDS.TOTAL };
